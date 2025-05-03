fdd858e20f98faebd29867b20e11dc952b88e468
ciberOn <classkiberone@gmail.com>
2025-05-03 18:53:26 +0300

    update2

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 1280
-canvas.height = 768
+canvas.width = 1920
+canvas.height = 1080
 
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
